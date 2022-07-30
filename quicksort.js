d5427c636640f480102f73c58cc8898d9b4ca07a
Rob Sameh <rob.sameh@gmail.com>
2022-07-30 15:55:46 -0700

    wrote out quicksort

---
+++ quicksort.js
@@ -0,0 +1,21 @@
+function quicksort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  let pivot = array[0];
+  
+  let left = []; 
+  let right = [];
+
+  for (let i = 1; i < array.length; i++) {
+    array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+  }
+
+  return quicksort(left).concat(pivot, quicksort(right));
+};
+
+let unsorted = [9, 2, 7, 12];
+let sorted = quicksort(unsorted);
+
+console.log('Sorted array', sorted);
